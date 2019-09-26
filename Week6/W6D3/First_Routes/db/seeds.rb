aa597aa994fa4532e8a3cc780fb6e9fa55dd3b7b
App Academy Student <student@appacademy.io>
2019-09-25 17:00:04 -0700

    Art Share API Start

--- Week6/W6D3/First_Routes/db/seeds.rb
+++ Week6/W6D3/First_Routes/db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(name: "Carly", email: "carly@yahoo.com")
-User.create!(name: "Paul", email: "paul@gmail.com")
-User.create!(name: "Jeremy", email: "jeremy@hotmail.com")
-User.create!(name: "Wolf", email: "wolfy@mypack.com")
+# User.create!(name: "Carly", email: "carly@yahoo.com")
+# User.create!(name: "Paul", email: "paul@gmail.com")
+# User.create!(name: "Jeremy", email: "jeremy@hotmail.com")
+# User.create!(name: "Wolf", email: "wolfy@mypack.com")
