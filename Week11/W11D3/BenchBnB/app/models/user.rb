8baac8ecabc825ef2e78b96cc6d81d5f31845a15
App Academy Student <student@appacademy.io>
2019-10-30 16:51:17 -0700

    bench first day

--- Week11/W11D3/BenchBnB/app/models/user.rb
+++ Week11/W11D3/BenchBnB/app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :password_digest, presence: true
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-  after_intialize :ensure_session_token
+  after_initialize :ensure_session_token
 
 
   def ensure_session_token 
